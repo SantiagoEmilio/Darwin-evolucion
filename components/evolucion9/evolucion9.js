da977d04125ae434ec6cc396f044351421236c49
Santiago <slopez@scl.edu.gt>
2024-08-13 20:32:53 -0600

    Ultima actualización 15/8/2024

--- components/evolucion9/evolucion9.js
+++ components/evolucion9/evolucion9.js
@@ -2,72 +2,59 @@ let DOM_evolucion9 = document.querySelector("#evolucion9");
 DOM_evolucion9.innerHTML = `
 
 <header class="estru">
- <div class="v9-titulo">
-<p class="h1b2">  
-  la evolución del deporte en automóvil</p>
-</div>
+  <div class="v9-titulo">
+    <p class="h1b2">  
+      La evolución del deporte en automóvil</p>
+  </div>
 </header>
 
 <div class="v9-general">
   <div class="info100">
- 
-    <h1> 1914: El Primer Carro Deportivo
- </h1>
-
-    <p> Vauxhall Prince Henry Sports Torpedo: Diseñado por Laurence Pomeroy, este modelo inglés es considerado el primer carro deportivo. Contaba con una velocidad máxima de 128 km/h, un motor de 4 litros con 24 caballos de fuerza y una caja de cambios de 4 velocidades.</p>
+    <h1>1894: Primera Carrera Automovilística</h1>
+    <p>Se organiza la primera carrera de automóviles de la historia, de París a Rouen, en Francia. Los coches eran en su mayoría impulsados por motores de vapor, gasolina o electricidad.</p>
     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Brooklands_1930.jpg/220px-Brooklands_1930.jpg" alt="" class="foto100">
- 
   </div>
 
-   <div class="info200">
-     <h1> Década de 1930: La Era de los Roadsters </h1>
- 
-     <p>Roadsters: Comienza a popularizarse este tipo de carros deportivos descapotables, diseñados para dos pasajeros y con un enfoque en la velocidad y la maniobrabilidad.</p>
-      <img src="https://cdn.topgear.es/sites/navi.axelspringer.es/public/media/image/2023/06/duesenberg-model-j-3069408.jpg?tf=3840x" alt="" class="foto200">
+  <div class="info200">
+    <h1>1906: Primer Gran Premio</h1>
+    <p>Se celebra el primer Gran Premio en Le Mans, Francia, organizado por el Automobile Club de Francia. Esta carrera marcó el inicio de las competiciones de automovilismo de velocidad.</p>
+    <img src="https://momentodeportivord.com/wp-content/uploads/2021/06/Primer-vehiculo-de-carrera.jpg" alt="" class="foto200">
   </div>
 
+  <div class="info300">
+    <h1>1923: Primera Carrera de las 24 Horas de Le Mans</h1>
+    <p>Se realiza la primera edición de las 24 Horas de Le Mans, una de las carreras de resistencia más famosas del mundo, que prueba la durabilidad y la eficiencia de los carros.</p>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlBaysXSAibZg1VFh5QHleRbXEJmFiQu8Hgg&s" alt="" class="foto300">
+  </div>
 
- <div class="info300">
-      <h1> Década de 1950: Nacimiento de los Muscle Cars </h1>
+  <div class="info400">
+    <h1>1950: Inicio del Campeonato Mundial de Fórmula 1</h1>
+    <p>Se celebra la primera temporada del Campeonato Mundial de Fórmula 1, comenzando con el Gran Premio de Gran Bretaña en Silverstone. La Fórmula 1 se convierte en la categoría reina del automovilismo.</p>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRi-3DukzlnrQgJOCDj0FR9ZtC4NrwPDJSVmQ&s" alt="" class="foto400">
+  </div>
 
-     <p>Muscle Cars: Surge en Estados Unidos una nueva categoría de carros deportivos con motores potentes y carrocerías robustas, como el Ford Mustang y el Chevrolet Camaro. Estos vehículos eran accesibles en precio y ofrecían altas prestaciones.</p>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6drPO3n7rafZ90JPNv6yWyyrSgCXOrJ_dtw&s" alt="" class="foto300">
- </div>
+  <div class="info500">
+    <h1>1970: Primer Campeonato de NASCAR</h1>
+    <p>La NASCAR (National Association for Stock Car Auto Racing) se convierte en la principal categoría de carreras en Estados Unidos, con Richard Petty ganando el primer campeonato bajo el nuevo sistema de puntos.</p>
+    <img src="https://phantom-marca-us.unidadeditorial.es/2049ed759d5122fbf913b0df1aa79460/resize/828/f/jpg/assets/multimedia/imagenes/2022/03/16/16474516935796.jpg" alt="" class="foto500">
+  </div>
 
+  <div class="info600">
+    <h1>2000: Era Moderna de la Fórmula 1</h1>
+    <p>La Fórmula 1 entra en una nueva era con avances significativos en la tecnología y la seguridad, junto con la introducción de motores más eficientes y regulaciones más estrictas.</p>
+    <img src="https://cdn-1.motorsport.com/images/amp/X0k79eD0/s6/f1-australian-gp-2007-lewis-ha.jpg" alt="" class="foto600">
+  </div>
 
-   <div class="info400">
-      <h1>Década de 1960: Introducción de los Superdeportivos </h1>
+  <div class="info700">
+    <h1>2014: Introducción de los Motores Híbridos en la Fórmula 1</h1>
+    <p>Se introducen los motores híbridos, combinando motores de combustión interna con sistemas de recuperación de energía, marcando un cambio hacia una mayor eficiencia energética.</p>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyJuCwbqcW5xXeraSIX5NceJdYe_BEXl3Fqw&s" alt="" class="foto700">
+  </div>
 
-      <p>Lamborghini Miura: Introducido en 1966, se considera el primer superdeportivo, con un motor V12 y un diseño innovador que influyó en la industria automotriz.</p>
-        <img src="https://img.remediosdigitales.com/59c29a/lamborghini-miura-p400/1366_2000.jpg" alt="" class="foto400">
-   </div>
-
-    <div class="info500">
-      <h1> Década de 1970: El Auge de los Gran Turismo </h1>
-
-      <p>Gran Turismo (GT): Esta categoría agrupa a carros deportivos más grandes y cómodos, diseñados para viajes largos en autopista. Ejemplos incluyen el Ferrari 365 GTB/4 y el Aston Martin V8.</p>
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzN1_QG9lcG0Wl5gaqsMunBMKakzfIZzBHcw&s" alt="" class="foto500">
-   
-    </div>
-
- <div class="info600">
-      <h1> Década de 1990: Innovaciones en Electrónica y Aerodinámica</h1>
-
-     <p>Electrónica y Aerodinámica: Los carros deportivos comienzan a integrar más tecnología, como sistemas de control de tracción y aerodinámica avanzada, mejorando tanto el rendimiento como la seguridad.</p>
-        <img src="https://www.avtotachki.com/wp-content/uploads/2021/03/7-4.jpg" alt="" class="foto600">
- </div>
-
- <div class="info700">
-     <h1> 2000 - 2010: Evolución de Motores y Transmisiones </h1>
-
-     <p>Motores y Transmisiones: Se popularizan los motores V6 en lugar de los tradicionales V8 o V10, así como las transmisiones automáticas que permiten cambios de marcha más rápidos y precisos.</p>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQi16hgS3Y2Zfvq9ql59iLzu6yDOmH3fyNBZA&s" alt="" class="foto700">
-    </div>
-
-    <div class="final00">
-        <h1>Autónomos y Sostenibles: La industria se dirige hacia carros deportivos que combinan conducción autónoma con motores eléctricos avanzados, manteniendo el enfoque en la velocidad y el rendimiento, pero con un menor impacto ambiental.</h1>
-        <img src="https://i.pinimg.com/736x/e7/6b/db/e76bdb2050d6dcb420fd05451f6db0ee.jpg" alt="" class="foto">
-    </div>
+  <div class="final00">
+    <h1>Esta línea de tiempo cubre algunos de los hitos más importantes en la historia del automovilismo deportivo, destacando la evolución desde las primeras carreras hasta las innovaciones tecnológicas más recientes.</h1>
+    <img src="https://s1.abcstatics.com/abc/www/multimedia/deportes/2024/02/13/ferrari-ok-U6038317349Z8G--1200x630@abc.jpg" alt="" class="foto">
+  </div>
 </div>
 `;
 
